903a135073167cea9e84f6db0dea4e493acd9ce3
문경현 <ghoflvhxj@naver.com>
2025-06-05 05:03:21 +0900

    솔루션, 엔진 필터 등 기타 환경파일 커밋

--- CopyDebug.bat
+++ CopyDebug.bat
@@ -1,9 +1,11 @@
 ﻿// 명령어		복사할 파일이 있는 위치			복사된 파일을 저장할 위치
 
-copy		.\Engine\*.h				.\Header\Engine\
+//copy		.\Engine\*.h					.\Header\Engine\
 
-copy		.\Engine\DirectXTK\*.h			.\Header\Engine\
+//copy		.\Engine\DirectXTK\*.h			.\Header\Engine\
 
-copy		.\Engine\x64\Debug\*.dll			.\Client\x64\Debug\Bin\
-copy		.\Engine\x64\Debug\*.lib			.\Libraries\Debug\
-xcopy 	.\Engine\x64\Debug\Shader		.\Client\Resources\Shader\	/e /h /k /y
+xcopy		.\Engine\Source\ThirdParty\ImGui\*.cpp	.\Client\Source\ThirdParty\ImGui\ /e /y
+copy		.\Engine\x64\Debug\*.dll				.\Client\x64\Debug\Bin\
+copy		.\Engine\x64\Debug\*.lib				.\Libraries\Debug\
+xcopy 	.\Engine\x64\Debug\Shader			.\Client\Resources\Shader\	/e /h /k /y
+
