6d703b119366e48a48b7b3108e2a63e1f4986cb2
문경현 <ghoflvhxj@naver.com>
2025-06-26 00:35:35 +0900

    스태틱 메시 액터 추가

---
+++ Engine/Source/GameFramework/StaticMeshActor/StaticMeshActor.cpp
@@ -0,0 +1,15 @@
+﻿#include "StaticMeshActor.h"
+#include "StaticMeshComponent.h"
+
+MStaticMeshActor::MStaticMeshActor()
+    : Actor()
+{
+    StaticMeshComp = std::make_shared<StaticMeshComponent>();
+    addComponent(ROOT_COMPONENT, StaticMeshComp);
+}
+
+void MStaticMeshActor::SetStaticMesh(const std::wstring& Path)
+{
+    StaticMeshComp->SetMesh(Path);
+}
+
---
+++ Engine/Source/GameFramework/StaticMeshActor/StaticMeshActor.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Actor.h"
+
+class StaticMeshComponent;
+
+class ENGINE_DLL MStaticMeshActor : public Actor
+{
+public:
+    MStaticMeshActor();
+
+public:
+    void SetStaticMesh(const std::wstring& Path);
+    std::shared_ptr<StaticMeshComponent> GetStaticMeshCompoent() { return StaticMeshComp; }
+protected:
+    std::shared_ptr<StaticMeshComponent> StaticMeshComp = nullptr;
+};
