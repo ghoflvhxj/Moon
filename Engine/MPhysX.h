c83dead86dc425f9fe4304a33596d323fcbf8971
문경현 <ghoflvhxj@naver.com>
2025-06-30 03:30:34 +0900

    상위 클래스 Physics가 추가되어 대응

--- Engine/MPhysX.h
+++ Engine/MPhysX.h
@@ -7,38 +7,6 @@
 #include "NvidiaPhysX/extensions/PxDefaultAllocator.h"
 using namespace physx;
 
-class ENGINE_DLL MPhysXObject : public MPhysicsObject
-{
-public:
-    MPhysXObject(std::shared_ptr<StaticMesh> InMesh, EPhysicsType InPhysicsType);
-    virtual ~MPhysXObject() = default;
-
-public:
-    virtual void SetPhysics(bool bEnable) override;
-    virtual void SetMass(float InMass) override;
-    virtual void SetPos(const Vec3& InPos) override;
-    virtual void SetScale(const Vec3& InScale) override;
-    virtual void SetGravity(bool bInGravity) override;
-    virtual void AddForce(const Vec3& InForce) override;
-    virtual void SetVelocity(const Vec3& InVelocity) override;
-    virtual void SetAngularVelocity(const Vec3& InVelocity) override;
-
-public:
-    virtual Vec3 GetPhysicsPos() override;
-    virtual Vec4 GetPhysicsRotation() override;
-
-public:
-    void AttachShape(PxShape* InShape);
-    bool IsValid() { return GetRigidActor() != nullptr; }
-
-protected:
-    physx::PxRigidActor* GetRigidActor();
-protected:
-    PxRigidStatic* RigidStatic = nullptr;
-    PxRigidDynamic* RigidDynamic = nullptr;
-    PxShape* Shape = nullptr;
-};
-
 class ENGINE_DLL MPhysX : public MPhysics
 {
 public:
@@ -66,4 +34,52 @@ protected:
 
 public:
     virtual bool AddPhysicsObject(std::shared_ptr<StaticMesh> InMesh, EPhysicsType InPhysicsType, std::unique_ptr<MPhysicsObject>& OutPhysicsObject) override;
+};
+
+class ENGINE_DLL MPhysXObject : public MPhysicsObject
+{
+public:
+    MPhysXObject(std::shared_ptr<StaticMesh> InMesh, EPhysicsType InPhysicsType);
+    virtual ~MPhysXObject() = default;
+
+public:
+    MPhysX* Get()
+    {
+        return static_cast<MPhysX*>(g_pPhysics.get());
+    }
+    PxPhysics* GetPhysX()
+    {
+        return Get()->Physics;
+    }
+    PxScene* GetScene()
+    {
+        return Get()->Scene;
+    }
+
+public:
+    virtual bool IsSimulating() override;
+public:
+    virtual void SetSimulate(bool bEnable) override;
+    virtual void SetMass(float InMass) override;
+    virtual void SetPos(const Vec3& InPos) override;
+    virtual void SetScale(const Vec3& InScale) override;
+    virtual void SetGravity(bool bInGravity) override;
+    virtual void AddForce(const Vec3& InForce) override;
+    virtual void SetVelocity(const Vec3& InVelocity) override;
+    virtual void SetAngularVelocity(const Vec3& InVelocity) override;
+
+public:
+    virtual Vec3 GetPhysicsPos() override;
+    virtual Vec4 GetPhysicsRotation() override;
+
+public:
+    void AttachShape(PxShape* InShape);
+    bool IsValid() { return GetRigidActor() != nullptr; }
+
+protected:
+    physx::PxRigidActor* GetRigidActor();
+protected:
+    PxRigidStatic* RigidStatic = nullptr;
+    PxRigidDynamic* RigidDynamic = nullptr;
+    PxShape* Shape = nullptr;
 };
