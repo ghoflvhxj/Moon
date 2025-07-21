b2437dc04b71c1c11466ad8b475e21ce3d7a5b86
문경현 <ghoflvhxj@naver.com>
2025-07-22 02:50:06 +0900

    PointLightActor 추가

---
+++ Engine/Source/GameFramework/PointLightActor/PointLightActor.cpp
@@ -0,0 +1,13 @@
+﻿#include "PointLightActor.h"
+#include "PointLightComponent.h"
+#include "GameFramework/BillboardComponent/BillboardComponent.h"
+
+MPointLightActor::MPointLightActor()
+{
+    PointLightComponent = std::make_shared<MPointLightComponent>();
+    addComponent(ROOT_COMPONENT, PointLightComponent);
+
+    VisualComponent = std::make_shared<MBillboardComponent>();
+    addComponent(TEXT("Asd"), VisualComponent);
+}
+
---
+++ Engine/Source/GameFramework/PointLightActor/PointLightActor.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Include.h"
+#include "Actor.h"
+
+class MPointLightComponent;
+class MBillboardComponent;
+
+class ENGINE_DLL MPointLightActor : public Actor
+{
+public:
+    MPointLightActor();
+
+protected:
+    std::shared_ptr<MPointLightComponent> PointLightComponent = nullptr;
+    std::shared_ptr<MBillboardComponent> VisualComponent = nullptr;
+};
