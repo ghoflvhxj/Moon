8eb7ca4a0f84109f517c91e1438229414703adda
문경현 <ghoflvhxj@naver.com>
2026-01-23 16:18:33 +0900

    포인트 라이트 스펙큘러

--- Engine/PointLightShader.hlsl
+++ Engine/PointLightShader.hlsl
@@ -86,11 +86,8 @@ PixelOut_LightPass main(PixelIn pIn)
 
     float3 specularFactor = pow(saturate(dot(PixelToCamera, direction)), 10.f);
     
-    //if (Bright > 0.f)
-    //{
-    //    pOut.lightSpecular = float4(specular.xyz * specularFactor * (1.f - ShadowFactor), 1.f);
-    //}
-
+    pOut.lightSpecular = float4(specular.xyz * specularFactor * NonShadow, 1.f);
+    
     /********************************
         디버깅
     ********************************/
